385240260620912567866941d7ebafd6edfae3da
EdoardoZappia <edoardozappia5@gmail.com>
2024-06-03 11:23:21 +0200

    m

--- SGD.py
+++ SGD.py
@@ -43,11 +43,11 @@ test_graph = create_test_graph(20)
 graph_cost = qaoa_maxcut_graph(test_graph, n_layers=2)
 print(f"Number of nodes: {test_graph.number_of_nodes()}")
 # Parameters are initialized to ones
-x = tf.Variable(tf.ones((2, 2)))
+x = tf.Variable(np.random.rand(2, 2))
 
 # We set the optimizer to be a Stochastic Gradient Descent
 opt = tf.keras.optimizers.SGD(learning_rate=0.01)
-step = 15
+step = 5
 
 # Training process
 steps = []
